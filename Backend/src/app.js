9b59e2c0ece2dca62b10a827ad479010486529ab
chandanbvminfotech-eng <chandan.bvminfotech@gmail.com>
2026-05-06 17:36:51 +0530

    latestcookiefix2.0

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:4173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
